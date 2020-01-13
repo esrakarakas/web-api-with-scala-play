cbc4ec51d72f38dee8a68ea16641c18f3813acac
esrakarakas <esrakarakas96@gmail.com>
2020-01-13 23:25:22 +0300

    Update 1.sql

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,11 +1,11 @@
 # Person schema
 
-# !Ups
-CREATE TABLE person (
-    id int(20) AUTO_INCREMENT,
-    name varchar(255),
-    lastName varchar(255),
-    phone1 varchar(255),
-    phone2 varchar(255),
-    PRIMARY KEY (id)
+# ---!Ups
+CREATE TABLE person(
+    id INT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(25),
+    lastName VARCHAR(25),
+    phone1 VARCHAR(25),
+    phone2 VARCHAR(25)
 );
+
